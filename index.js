994e7c654f73558ff7c034e4e14641e62fa7c188
jamilagastar <jamilagastar@gmail.com>
2022-04-11 20:54:57 +1000

    Practiced functions

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Follow along with the examples here
+//function sayHelloToIsabel() {
+//    console.log("Hello, Isabel!")
+//}
+
+//sayHelloToIsabel();
+
+//function sayHelloToSofia () {
+//   console.log("Hello, Sofia!")
+//}
+
+//sayHelloToSofia();
+
+//function sayHelloToBrendan() {
+//    console.log("Hello, Brendan")
+//}
+
+//sayHelloToBrendan();
+
+//function doSomething(thing) {
+//    console.log(thing);
+//  }
+  
+//  doSomething('anything');
+
+//function sayHelloTo(firstName) {
+//    console.log(`Hello, ${firstName}!`)
+//}
+
+//sayHelloTo('Isabel');
+//sayHelloTo("Jane");
+//sayHelloTo("R2-D2");
+//sayHelloTo(1);
+
+//function say(greeting, firstName) {
+//    console.log(`${greeting}, ${firstName}!`);
+//}
+
+//console.log(say("Hello", "Sofia"));
+
+//say("Julio", "hello");
+
+//function add(x, y) {
+//    return x + y;
+//}
+
+//console.log(add(1, 2));
+
+function say(greeting, firstName) {
+    console.log('I was called!');
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("Howdy", "partner"));
